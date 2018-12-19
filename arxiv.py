3a60d5d93dfc9a61b3bbccbf5544679d2e5fe843
Mitsuharu Uemoto <uemoto@ccs.tsukuba.ac.jp>
2018-12-19 10:10:46 +0900

    hotfix

--- arxiv.py
+++ arxiv.py
@@ -7,35 +7,42 @@ import tempfile
 import shutil
 import chardet
 
-
 from tex2csv import tex2csv
 
 ptn_number = re.compile(r'(\d+\.\d+)')
 
+
+    
+
+
 if __name__ == '__main__':
     
     parser = optparse.OptionParser()
-    parser.add_option('-d', '--directory', dest='directory', 
-        type=str, default=os.curdir, help='Output directory')            
+    parser.add_option('-r', '--reload', dest='reload', 
+        action='store_true', default=False, help='Force reloading')            
     opts, args = parser.parse_args()
     
     for item in args:
         m_number = ptn_number.search(item)
         if m_number:
             url = 'https://arxiv.org/e-print/' + m_number.group(1)
+            file_tar_gz = os.path.abspath("%s.tar.gz" % m_number.group(1))
+            
+            if opts.reload or not os.path.file(file_tar_gz):
+                print('Retrieving %s ...' % url)
+                urllib.request.urlretrieve(url, file_tar_gz)
+            else:
+                print('%s already exists...' % file_tar_gz)
             
             with tempfile.TemporaryDirectory() as tmpdir:
-                file_tgz = os.path.join(tmpdir, m_number.group(1) + '.tgz')
+                print('Extracting %s ...' % file_tar_gz)
+                subprocess.call(['tar', '-zxf', file_tar_gz], cwd=tmpdir)
             
-                print('Retrieving %s ...' % url)
-                urllib.request.urlretrieve(url, file_tgz)
-            
-                print('Extracting %s ...' % file_tgz)
-                subprocess.call(['tar', '-zxf', file_tgz], cwd=tmpdir)
-                
-                for title in os.listdir(tmpdir):
-                    if os.path.splitext(title)[-1] == '.tex':
-                        file_tex = os.path.join(tmpdir, title)
+                for basedir, _, filelist in os.walk(tmpdir):
+                    for title in filelist:
+                        if os.path.splitext(title)[-1] == '.tex':
+                            file_tex = os.path.join(basedir, title)
+                            
                         
                         file_tex2 = os.path.join(
                             opts.directory, m_number.group(1) + '.tex'
@@ -45,6 +52,7 @@ if __name__ == '__main__':
                             opts.directory, m_number.group(1) + '.csv.gz'
                         )    
                         
+                        print("Reading ")
                         with open(file_tex, 'rb') as fh:
                             buff = fh.read()
                         
@@ -53,11 +61,11 @@ if __name__ == '__main__':
                         buff2 = buff.decode(cd['encoding'])
                         
                         print('Converting %s ...' % file_tex2)
-                        with open(file_tex2, 'w') as fh:
-                            fh.write(buff2)
-                        
-                        tex2csv(file_tex, file_csv)
-                        break
+                    with open(file_tex2, 'w') as fh:
+                        fh.write(buff2)
+                    
+                    tex2csv(file_tex, file_csv)
+                    break
                         
                 
                 
--- tex2csv.py
+++ tex2csv.py
@@ -3,12 +3,36 @@ import nltk
 import gzip
 import sys
 import csv
+import tempfile
 
+def clean_tex(text):
+    # Removing outside of document section:
+    tag_doc1 = r"\begin{document}"
+    tag_doc2 = r"\end{document}"
+    pos_doc1 = text.find(tag_doc1)
+    if 0 <= pos_doc1:
+        pos_doc2 = text.find(tag_doc2) + len(tag_doc2)
+        text = text[pos_doc1:pos_doc2]
+        print("Cut doc")
+    
+    # Removing thebibliography section
+    tag_bib1 = r"\begin{thebibliography}"
+    tag_bib2 = r"\end{thebibliography}"
+    pos_bib1 = text.find(tag_bib1)
+    if 0 <= pos_bib1:
+        pos_bib2 = text.find(tag_bib2) + len(tag_bib2)
+        text = text[:pos_bib1] + text[pos_bib2:]
+        print("Cut bib")
+    return text
 
 def tex2csv(file_tex, file_csv, ncolumn=24):
 
-    code = subprocess.check_output(["pandoc", "-i", file_tex, "--to", "plain"])
-    text = code.decode('utf8')
+    with tempfile.NamedTemporaryFile(suffix='.tex') as fh_tmp:
+        with open(file_tex) as fh_tex:
+            fh_tmp.write(clean_tex(fh_tex.read()).encode('utf-8'))
+        fh_tmp.flush()
+        code = subprocess.check_output(["pandoc", "-i", fh_tmp.name, "--to", "plain"])
+    text = code.decode('utf-8')
 
     buff = []
     for i, tmp1 in enumerate(text.split("$$")):
